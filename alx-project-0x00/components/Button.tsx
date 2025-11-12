cd660f3d53f5f3dd2e2c794b9ea864a1657a58e5
solomon54 <solomontsehay54@gmail.com>
2025-11-12 22:02:05 +0300

    Create a button component that accepts title and styles as an argument.

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,6 @@
+const Button:React.FC<ButtonProps>=({size,shape})=>{
+return(
+    <button className={` ${size==="large"?"px-6 py-3 text-lg":"px-3 py-1 text-sm"} ${shape==="rounded"?"rounded-full":"rounded-md"} bg-blue-500 text-white font-semibold`}>{size}</button>
+)
+}
+export default Button;
