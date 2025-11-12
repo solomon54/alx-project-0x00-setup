1fbbfe07f7e7338087cd84d6d18e002b6438409a
solomon54 <solomontsehay54@gmail.com>
2025-11-12 20:57:30 +0300

    rendering components

--- alx-project-0x00/pages/about.tsx
+++ alx-project-0x00/pages/about.tsx
@@ -1,8 +1,10 @@
 const About: React.FC =  () => {
   return (
     <div>
-   <h1 className="text-xl font-extralight">About Page</h1>
+   <h1 className="font-extralight flex justify-center shadow-amber-500 shadow-2xl text-4xl mt-10 m-20 text-amber-700 rounded-2xl">About Page</h1>
+   <button className="font-semibold text-amber-600 bg-amber-300 shadow-2xl rounded-2xl py-2 px-4 ml-10 active:text-2xl cursor-pointer ">Disaible</button>
     </div>
   )
 }
+
 export default About;
