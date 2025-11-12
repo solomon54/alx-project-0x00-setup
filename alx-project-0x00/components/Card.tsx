c9487f3e874c4fd00c8274c732b5af2809a18df8
solomon54 <solomontsehay54@gmail.com>
2025-11-12 21:39:04 +0300

    Typing functional components'

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -9,6 +9,8 @@ const Card: React.FC = () => {
 
  return (
     <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
+    
+
       <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
       <div className="p-2 flex gap-2 mt-2">
         <Pill title="Top Villa" />
@@ -20,6 +22,8 @@ const Card: React.FC = () => {
           <h3 className=" font-semibold text-[22px]">Villa Arrecife Beach House</h3>
           <p className=" font-medium text=[17px] text-[#929292]">Sideman, Bali, Indonesia</p>
         </div>
+  
+
         <div className="flex items-center">
           <Image src={STAR_IMAGE} alt="star" />
           <p className=" font-medium text=[17px] ml-2">4.76</p>
@@ -62,6 +66,7 @@ const Card: React.FC = () => {
 
       </div>
     </div>
+    
   )
 }
 
--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,11 +1,12 @@
 import { title } from "process";
+import { pillprops } from "@/interfaces";
 
-const Pill: React.FC = () => {
+const Pill: React.FC<PageProps> = ({ title }) => {
   return (
     <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className=" text-sm ">Title</p>
+      <p className=" text-sm ">{title}</p>
     </div>
   )
-}
+};
 
 export default Pill;
